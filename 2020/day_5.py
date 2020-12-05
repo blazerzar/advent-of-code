f09ae0eb955515498b3de7de03486a204e0feaf9
Blaž Erzar <blaz.erzar@gmail.com>
2020-12-05 08:51:14 +0100

    Updated day 5 to use binary numbers for seat IDs

--- 2020/day_5.py
+++ 2020/day_5.py
@@ -1,4 +1,3 @@
-import math
 import os
 
 
@@ -9,27 +8,12 @@ def my_seat(seat_IDs: list) -> int:
     return seats.difference(set(seat_IDs)).pop()
 
 
-def seat_location(boarding_pass: str) -> tuple:
-    """Calculate seat row and column from a boarding pass"""
-    row_min, row_max = 0, 127
-    column_min, column_max = 0, 7
-    for i, c in enumerate(boarding_pass):
-        if i < 7:
-            if c == "F":
-                row_max = row_min + int((row_max - row_min) / 2)
-            elif c == "B":
-                row_min = row_min + math.ceil((row_max - row_min) / 2)
-        else:
-            if c == "L":
-                column_max = column_min + int((column_max - column_min) / 2)
-            elif c == "R":
-                column_min = column_min + math.ceil(
-                    (column_max - column_min) / 2)
-    return row_min, column_min
-
-
-def seat_ID(row: int, column: int) -> int:
-    return row * 8 + column
+def seat_ID(boarding_pass: str) -> int:
+    binary = (boarding_pass.replace("F", "0")
+                           .replace("B", "1")
+                           .replace("L", "0")
+                           .replace("R", "1"))
+    return int(binary, 2)
 
 
 def read_file(file_name: str) -> list:
@@ -39,21 +23,15 @@ def read_file(file_name: str) -> list:
 
 
 def test():
-    assert seat_location("FBFBBFFRLR") == (44, 5)
-    assert seat_location("BFFFBBFRRR") == (70, 7)
-    assert seat_location("FFFBBBFRRR") == (14, 7)
-    assert seat_location("BBFFBBFRLL") == (102, 4)
-    assert seat_ID(44, 5) == 357
-    assert seat_ID(70, 7) == 567
-    assert seat_ID(14, 7) == 119
-    assert seat_ID(102, 4) == 820
+    assert seat_ID("BFFFBBFRRR") == 567
+    assert seat_ID("FFFBBBFRRR") == 119
+    assert seat_ID("BBFFBBFRLL") == 820
 
 
 def main():
     test()
     boarding_passes = read_file("inputs/input5.txt")
-    seat_IDs = [seat_ID(*seat_location(boarding_pass))
-                for boarding_pass in boarding_passes]
+    seat_IDs = [seat_ID(boarding_pass) for boarding_pass in boarding_passes]
     print(max(seat_IDs))
     print(my_seat(seat_IDs))
 
