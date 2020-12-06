39dc178c5ae5ae22d6ae3c1ce533cc029a2bd81a
Blaž Erzar <blaz.erzar@gmail.com>
2020-12-06 17:27:05 +0100

    Updated day 6 to use splitting by an empty line

--- 2020/day_6.py
+++ 2020/day_6.py
@@ -7,20 +7,11 @@ def read_file(file_name: str) -> Tuple[list, list]:
     Read file and create two lists of sets, one contains answers answered by
     at least one person and the other by everyone
     """
-    answers_part1 = []
-    answers_part2 = []
-    index = 0
-    with open(os.path.join(os.path.dirname(__file__), file_name), "r") as f:
-        for line in f:
-            answers = {c for c in line.strip()}
-            if not answers:
-                index += 1
-            elif answers and len(answers_part1) <= index:
-                answers_part1.append(set())
-                answers_part2.append(answers)
-            if answers:
-                answers_part1[-1] |= answers
-                answers_part2[-1] &= answers
+    groups = [line.split() for line in open(os.path.join(
+              os.path.dirname(__file__), file_name), "r")
+              .read().split("\n\n")]
+    answers_part1 = [set.union(*(set(a) for a in g)) for g in groups]
+    answers_part2 = [set.intersection(*(set(a) for a in g)) for g in groups]
     return answers_part1, answers_part2
 
 
